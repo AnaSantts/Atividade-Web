b22add73217e74f0134442a40da09be692095f59
Ana Beatriz <ana.delfino@academico.ifpb.edu.br>
2024-10-28 22:28:34 -0300

    Adicionando a questão 14 de fundamentos

---
+++ Documents/Atividade-avaliativa/Fundamentos/fundamentos-questao14.js
@@ -0,0 +1,9 @@
+let frase = "Às vezes, o caminho certo não é o mais fácil.";
+
+const palavra = "caminho"; 
+
+console.log("ops");
+
+const presente = frase.includes(palavra);
+
+console.log(presente);
