0b21b11580b6b074a8fe449f5b04580f8c8aef38
Ana Beatriz <ana.delfino@academico.ifpb.edu.br>
2024-10-28 22:23:51 -0300

    Adicionando a questão 4 de fundamentos

---
+++ Documents/Atividade-avaliativa/Fundamentos/fundamentos-questao4.js
@@ -0,0 +1,6 @@
+let numero1 = 40; 
+let numero2 = 10; 
+
+const resultado = (numero1 === 30 || numero2 === 30 || (numero1 + numero2 === 30) || (numero1 === 12 && numero2 === 12));
+
+console.log(resultado);
