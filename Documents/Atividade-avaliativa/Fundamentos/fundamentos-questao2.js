cee7715e14ffcbf339c6f0f487b989ca1041433c
Ana Beatriz <ana.delfino@academico.ifpb.edu.br>
2024-10-28 22:13:24 -0300

    Adicionando questão 2 de fundamentos

---
+++ Documents/Atividade-avaliativa/Fundamentos/fundamentos-questao2.js
@@ -0,0 +1,9 @@
+const a = 4; 
+const b = 6;
+const c = 8; 
+
+const p = (a + b + c) / 2;
+
+const area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+console.log(`A área do triângulo é: ${area}`);
